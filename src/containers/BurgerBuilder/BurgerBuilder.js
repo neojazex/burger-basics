b53cf8b81cef5b620ae78cac075ce483062217b9
neojazex <jason.e.j.hall@gmail.com>
2019-09-29 20:17:38 +1000

    162. Removing Ingredients Safely

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,42 +29,46 @@ class BurgerBuilder extends Component {
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0) {
+            return;
+        }
+
         const updatedCount = oldCount+1;
         const updatedIngredients = {...this.state.ingredients};
         updatedIngredients[type] = updatedCount;
         const priceAddition = INGREDIENT_PRICES[type]
         const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice + priceAddition;
+        const newPrice = oldPrice - priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     removeIngredientHandler = (type) => {
-        //broken
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount-1;
-        const skipSubtraction = false;
-        if(updatedCount < 0) {
-            updatedCount = 0;
-            skipSubtraction = true;
-        }
         const updatedIngredients = {...this.state.ingredients};
         updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type]
         const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice;
-        if(!skipSubtraction) {
-            const priceAddition = INGREDIENT_PRICES[type]
-            newPrice = newPrice - priceAddition;
-        }
+        const newPrice = oldPrice + priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     render () {
+
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        for( let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
         return (
             <React.Fragment>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}/>
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}/>
             </React.Fragment>
         );
     }
