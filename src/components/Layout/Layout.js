39a3ac5f5bfa95bf5dabd92105fd0ecdee11fc64
neojazex <jason.e.j.hall@gmail.com>
2019-10-20 17:32:40 +1100

    178. Adding a Sidedrawer Toggle Button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,11 +12,16 @@ class Layout extends Component {
     sideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false });
     }
+    
+    sideDrawerToggleHandler = () => {
+            this.setState((prevState) => { return {showSideDrawer: !prevState.showSideDrawer };
+        });
+    }
 
     render() {
         return (
             <React.Fragment>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
                 <SideDrawer open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
