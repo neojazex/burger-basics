d149b7041efd817827eca93b0c951315778f105c
jason <jason@vintrace.com>
2019-10-24 08:13:11 +1100

    Config + rename

--- src/components/Navigation/SideDrawer/DrawerToggle/Drawertoggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/Drawertoggle.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import classes from './DrawerToggle.module.css';
 
+//named as DrawerToggle.js -- mind the case
+
 const drawerToggle = (props) => (
     <div className={classes.DrawerToggle} onClick={props.clicked}>
         <div></div>
