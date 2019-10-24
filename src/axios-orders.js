90f1d390af646d3aee1ff3f4d92d0be446d0f694
jason <jason@vintrace.com>
2019-10-25 07:46:30 +1100

    210. Sending a POST Request

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL= 'https://react-burger-builder-f14cc.firebaseio.com/'
+    baseURL: 'https://react-burger-builder-f14cc.firebaseio.com/'
 });
 
 export default instance;
